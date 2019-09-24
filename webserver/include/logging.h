20191a17b6ff0c177fb32d5009827940bcac2c22
Christoffer Åleskog <christoffer5511@gmail.com>
2019-09-24 20:50:05 +0200

    Logging done?

--- webserver/include/logging.h
+++ webserver/include/logging.h
@@ -1,7 +1,15 @@
 #ifndef LOGGING_H
 #define LOGGING_H
 
-void log(char* format, ...);
-void log_err(char* format, ...);
+int logging_file;
+
+void logging_log_to_file(int to_file);
+void _logging_log_f(const char* path, char* ip, char* userid, char* time, char* request, int response_code, char* size_in_bytes, char* referer, char* user_agent);
+void _logging_log(int pri, char* ip, char* userid, char* time, char* request, int response_code, int size_in_bytes, char* referer, char* user_agent);
+void logging_log(char* ip, char* userid, char* time, char* request, int response_code, int size_in_bytes);
+void logging_log_err(char* ip, char* userid, char* time, char* request, int response_code, int size_in_bytes);
+
+void logging_log_clg(char* ip, char* userid, char* time, char* request, int response_code, int size_in_bytes, char* referer, char* user_agent);
+void logging_log_err_clg(char* ip, char* userid, char* time, char* request, int response_code, int size_in_bytes, char* referer, char* user_agent);
 
 #endif
