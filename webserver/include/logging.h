9a2a94204512f29b0455106db7928525b6ef1532
Christoffer Åleskog <christoffer5511@gmail.com>
2019-09-24 15:07:59 +0200

    Started on the Logging

--- webserver/include/logging.h
+++ webserver/include/logging.h
@@ -1,6 +1,7 @@
 #ifndef LOGGING_H
 #define LOGGING_H
 
-
+void log(char* format, ...);
+void log_err(char* format, ...);
 
 #endif
--- webserver/src/logging.c
+++ webserver/src/logging.c
@@ -1,2 +1,25 @@
 #include "../include/logging.h"
+#include <syslog.h>
+#include <stdarg.h>
 
+int check(char* str)
+{
+    
+}
+
+void log(char* format, ...)
+{
+    while (format)
+    {
+        if (check("%s"))
+        {
+
+        }
+    }
+    
+}
+
+void log_err(char* format, ...)
+{
+
+}
