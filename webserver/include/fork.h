9d28ed120fedb5045677f870d1e5e599c9975956
rschuemann <rschuemann@stud.hs-bremen.de>
2019-10-06 14:34:31 +0200

    wrote fork(untested)

--- webserver/include/fork.h
+++ webserver/include/fork.h
@@ -1,6 +1,30 @@
 #ifndef FORK_H
 #define FORK_H
 
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "../include/socket.h"
+#include "../include/request.h"
+#include "../include/response.h"
+#include "../include/array.h"
+
+Array fork_array;
+
+typedef struct fork_array_info
+{
+	int id;
+	pid_t p;
+    int free_fork;
+    int kill_fork;
+} Fork_array_info;
+
+typedef struct fork_args
+{
+	int id;
+	Client client;
+} Fork_args;
+
 void exec_preforking();
 
 #endif
--- webserver/src/fork.c
+++ webserver/src/fork.c
@@ -1,6 +1,94 @@
 #include "../include/fork.h"
 
+int *client_fork(void *args)
+{
+	int fork_id = ((Fork_args *)args)->id;
+	Client *client = &((Fork_args *)args)->client;
+
+	printf("\n%d: Start Connection!\n", ((Fork_args *)args)->id);
+
+	Request_t request = request_received(client);
+	gather_response_information(&request, client);
+	free_headers(&request.headers);
+
+	shutdown(client->socket, SHUT_WR);
+	return 0;
+}
+
 void exec_preforking()
 {
-	//code here
+	array_create_array(&fork_array, sizeof(Fork_array_info), 6);
+	pid_t origin = getpid(void);
+	pid_t p;
+	p = fork();
+	p = fork();
+	printf("pid: %d", p);
+
+	int running = 1;
+	while (shoudStopRunning(running))
+	{
+		if (p != origin)
+		{
+			Fork_array_info info;
+
+			info.free_fork = 1;
+
+			if (fork_array[p].element)
+			{
+				info = fork_array[p].element;
+				if (info.kill_fork == 0)
+				{
+					array_remove_element(fork_array, p);
+					exit(0);
+				}
+				fork_array[p].element = info;
+			}
+			else
+			{
+				info.id = p;
+				info.kill_fork = 1;
+				array_push_element(fork_array, info);
+			}
+			int accept_connection;
+			Client client = connectToClient(&accept_connection);
+			if (accept_connection == 1)
+			{
+				Fork_args *args = malloc(sizeof(Fork_args));
+				args->client = client;
+				int rc = client_fork((void *)args, info);
+
+				if (rc)
+				{
+					fprintf(stderr, "ERROR: Could not handle clientrequest.");
+					exit(-1);
+				}
+				info.free_fork = 0;
+				fork_array[p].element = info;
+			}
+		}
+		else
+		{
+			int check_fork;
+			int busy_fork;
+			for (fork_array[check_fork].element)
+			{
+				Fork_array_info check = fork_array[check_fork].element;
+				if (check.free_fork == 1)
+				{
+					busy_fork++;
+				}
+				check_fork++;
+			}
+			if (busy_fork == fork_array.size)
+			{
+				p = fork();
+			}
+			else
+			{
+				Fork_array_info toKill = fork_array[fork_array.size].element;
+				toKill.kill_fork = 0;
+			}
+		}
+	}
+	request_stop_reciving_data = 1;
 }
