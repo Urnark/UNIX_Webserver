4eb9433609d667bdaa6cb9fb3e339cc5898506e1
rschuemann <rschuemann@stud.hs-bremen.de>
2019-10-07 09:18:16 +0200

    split up main

--- webserver/src/main.c
+++ webserver/src/main.c
@@ -13,28 +13,6 @@
 
 #define START_NUM_THREADS 5
 
-typedef struct thread_args
-{
-	int id;
-	Client client;
-} Thread_args;
-
-void *client_thread(void *args)
-{
-	int thread_id = ((Thread_args *)args)->id;
-	Client *client = &((Thread_args *)args)->client;
-
-	printf("\n%d: Start Connection!\n", ((Thread_args *)args)->id);
-
-	Request_t request = request_received(client);
-	gather_response_information(&request, client);
-	free_headers(&request.headers);
-
-	//close(client->socket);
-	shutdown(client->socket, SHUT_WR);
-	thread_manager_exit_thread(args);
-}
-
 int shoudStopRunning(int running)
 {
 	// For checking if the server should be terminated.
@@ -59,44 +37,6 @@ int shoudStopRunning(int running)
 		return 0;
 	}
 	return 1;
-}
-
-void start_server(int port, int log, int deamon, int setting)
-{
-	logging_log_to_file(log);
-
-	// Initilize the server
-	set_ip_type(0);
-	set_port(port);
-	set_protocol(0);
-	createSocket(10);
-	setToNonBlocking();
-
-	thread_manager_init_threads(START_NUM_THREADS);
-
-	request_init();
-
-	int running = 1;
-	while (shoudStopRunning(running))
-	{
-		int accept_connection;
-		Client client = connectToClient(&accept_connection);
-		if (accept_connection == 1)
-		{
-			Thread_args *args = malloc(sizeof(Thread_args));
-			args->client = client;
-			int rc = thread_manager_new_thread(client_thread, (void *)args);
-			if (rc)
-			{
-				fprintf(stderr, "ERROR: pthread_create() returned %d\n", rc);
-				exit(-1);
-			}
-		}
-	}
-	request_stop_reciving_data = 1;
-
-	thread_manager_terminate_threads();
-	closeServer();
 }
 
 int read_int_from_file(char *config_line)
