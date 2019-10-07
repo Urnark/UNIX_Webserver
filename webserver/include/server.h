c2b745d93300a35ff79d44872a567fdc8c178d97
Christoffer Åleskog <christoffer5511@gmail.com>
2019-10-07 13:44:50 +0200

    Fixed merge

--- webserver/include/server.h
+++ webserver/include/server.h
@@ -1,8 +1,12 @@
 #ifndef SERVER_H
 #define SERVER_H
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <sys/time.h>
+#include <errno.h>
 
 #include "../include/threadManager.h"
 #include "../include/socket.h"
@@ -16,8 +20,12 @@ typedef struct thread_args
 {
 	int id;
 	Client client;
+	int use_jail;
     int is_fork;
 } Thread_args;
+
+void start_server(int port, int log, int deamon, int setting, int use_jail);
+void change_chroot();
 
 
 #endif
